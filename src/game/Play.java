4e91f2f59dbffbb881579807790d7a3b3f6d3efc
SvenKt <sv-kujat@t-online.de>
2015-02-20 10:08:35 +0100

    new init after restart

--- src/game/Play.java
+++ src/game/Play.java
@@ -18,20 +18,24 @@ public class Play extends BasicGameState{
 	}
 	
 	public void init(GameContainer gc, StateBasedGame sbg) {
-		try {
-			player = new Player(100, 100, 3);
-			gameObjects.add(player);
-			setZombie(300,200);
-			setZombie(500,200);
-			setZombie(600,600);
-			setZombie(350,300);
-			setZombie(500,250);
-			setZombie(600,500);
-			
 
-		} catch (SlickException e) {
-			e.printStackTrace();
-		}
+	}
+
+	
+	@Override
+	public void enter(GameContainer container, StateBasedGame game) throws SlickException {
+		gameObjects.clear();
+		
+		player = new Player(100, 100, 3);
+		gameObjects.add(player);
+		setZombie(300,200);
+		setZombie(500,200);
+		setZombie(600,600);
+		setZombie(350,300);
+		setZombie(500,250);
+		setZombie(600,500);
+		super.enter(container, game);
+
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg,
@@ -40,21 +44,28 @@ public class Play extends BasicGameState{
 		// Render alle GameObjecte
 		for(GameObject gameObject : Play.gameObjects) {
 			gameObject.render();
+			//g.draw(gameObject.getHitbox());
 		}
 	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int arg1) {
+		
 		if(player.isPlayerDead()){
 			sbg.enterState(Game.MENU,new FadeOutTransition(Color.white), new FadeInTransition(Color.white));
 		}
 		
 		player.checkInputs(gc.getInput());
 		
-		for(GameObject gameObject : Play.gameObjects) {
-			gameObject.update();
-		}
+		updateAllObjects();
 		checkZombieDead();
-		System.out.println(scores.getScore());	
+		//checkHeartsLost();
+		checkPlayerAlive();
+		scores.addScore();
+		System.out.println(scores.getScore());
+		
+		
+		
+	
 	}
 	
 	public int getID() {
@@ -70,11 +81,18 @@ public class Play extends BasicGameState{
 		
 	}
 	
+	public void updateAllObjects(){
+		for(GameObject gameObject : Play.gameObjects) {
+			gameObject.update();
+		}
+	}
+
 	public void checkZombieDead(){
 		if(getroffenerZombie != null) {
 			Play.gameObjects.remove(getroffenerZombie);
 			Play.gameObjects.remove(kugel);
 			killingCounter++;
+			//MyGame.gameObjects.remove(this);
 			getroffenerZombie = null;
 			try {
 				if (killingCounter % 5 == 0){
@@ -87,13 +105,41 @@ public class Play extends BasicGameState{
 				e.printStackTrace();
 			}
 		}
-	}	
+	}
+	
+	/*
+	public void checkHeartsLost(){
+		if(verlorenesHerz != null) {
+			Play.gameObjects.remove(verlorenesHerz);
+			Healthbar.lifes--;
+			if (Healthbar.lifes == 0){
+				playerDead=true;
+			} else{
+				verlorenesHerz = null;
+			}
+		}
+	}
+	*/
+	
+	public void checkPlayerAlive(){
+		if (playerDead == true){
+			System.out.println("Player dead!!!");
+		}
+	}
+	
+	public void setZombie2(int x, int y) {
+		throw new UnsupportedOperationException();
+	}
 
 	private int state;
 	public static Score scores = new Score();
+	public static boolean playerDead = false;
 	private int killingCounter = 0;
 	public static Player player;
 	public static List<objects.GameObject> gameObjects = new ArrayList<GameObject>();
 	public static Zombie getroffenerZombie = null;
 	public static Shot kugel = null;
+	/*public static heart verlorenesHerz = null;
+	public static Collection<heart> hearts = new ArrayList<heart>();
+	*/
 }
